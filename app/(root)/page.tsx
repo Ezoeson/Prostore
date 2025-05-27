2ec0badeb67eaf2557eb4e48cac18a87d4fce2c3
Hasina ezoeson <nambinintsoa6017@gmail.com>
2025-05-27 20:43:22 +0300

    Ajout de la liste des produits et des composants associés pour la page d'accueil

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/db/sample-data';
+
 // const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const Homepage = () => {
- 
-
-  return <>TEST</>;
+  return (
+    <>
+      <ProductList data={sampleData.products} title='Newest Arrivals' limit={4} />
+    </>
+  );
 };
 
 export default Homepage;
